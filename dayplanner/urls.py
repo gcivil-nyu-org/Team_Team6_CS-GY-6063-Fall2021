0d85487c7c14f60dd7ec51fbb4c85aec02b6c7e5
Samuel Ballan <sgb4622@gmail.com>
2021-10-09 23:34:18 -0400

    wire up landing page

--- dayplanner/urls.py
+++ dayplanner/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
+from . import views
+
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
 ]
